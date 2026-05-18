dcc25cd00543e41f77ef3da50cdb8d3d8bc694de
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-05-18 20:21:34 +0530

    Assignment2

---
+++ Assignment_2/Q1.py
@@ -0,0 +1,5 @@
+# Write a program that takes salary as input. Using conditional statements, calculate the final tax rate based on these rules:
+# •Ifsalary<30,000→5%
+# •Ifsalaryis30,000–70,000→15%
+# •Ifsalary>70,000→25%
+
