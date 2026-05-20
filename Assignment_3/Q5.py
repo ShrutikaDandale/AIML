7239c53e1dedac0965e842c9fe2f828aa02745a4
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-05-20 19:55:48 +0530

    Assignment3

--- Assignment_3/Q5.py
+++ Assignment_3/Q5.py
@@ -7,3 +7,49 @@
 # B - Update marks 
 # C - Search for a student
 # D - Display all students and marks
+
+info = {
+    "shanku": 9.67,
+    "pitu": 9.87,
+    "kiki": 8.32
+}
+
+key = input("enter the key: ")
+
+# A
+if(key == "A"):
+         name = input("enter new student name")
+         marks = float(input("Enter new marks: "))
+         info.update({
+                name:marks
+    })
+         print(info)
+
+
+# B
+elif(key == "B"):
+    name = input("Enter new student name: ")
+    marks = float(input("Enter new marks: "))
+
+    info[name] = marks 
+    print(info)
+
+# C
+elif(key == "C"):
+
+    name = input("Enter student name to search: ")
+
+    if name in info:
+        print(name, "marks are:", info[name])
+    else:
+        print("Student not found")
+
+
+# D
+elif(key == "D"):
+
+   print(info)
+
+
+else:
+    print("Invalid key")     
