0b2c7608a7c45da0d3a57f817050d60450b03c03
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-05-20 16:44:42 +0530

    Assignment3

--- Assignment_3/Q3.py
+++ Assignment_3/Q3.py
@@ -1,4 +1,10 @@
 # Input two lists of integers from the user. Merge them into one list and sort the result.
-numbers = input("Enter numbers separated by space: ").split()
 
-print(numbers)
+list1 = list(map(int, input("Enter list1: ").split()))
+list2 = list(map(int, input("Enter list2: ").split()))
+
+result = list1 + list2
+
+result.sort()
+
+print(result)
