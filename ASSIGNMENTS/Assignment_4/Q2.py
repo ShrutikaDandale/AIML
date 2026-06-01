83100d5b6d4c808e1f46d4222aaa3adbb63abc2a
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-01 19:09:30 +0530

    Assignment

---
+++ ASSIGNMENTS/Assignment_4/Q2.py
@@ -0,0 +1,39 @@
+# Create a class Book with the following attributes:
+# •title
+# •author
+# •list of reviews
+#  And add methods to:
+# •add a new review
+# •count reviews 
+# •display all reviews
+
+class Book:
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.reviews = []
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def count_reviews(self):
+        print("Total reviews:", len(self.reviews))
+
+    def display_reviews(self):
+        for review in self.reviews:
+            print(review)
+
+
+b1 = Book("Python Basics", "ABC")
+
+b1.add_review("Good")
+b1.add_review("Excellent")
+
+b1.count_reviews()
+b1.display_reviews()
+
+
+
+
+    
