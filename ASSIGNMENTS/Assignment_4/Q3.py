e96b7f71224a8bcd0e7e3a7ee896a48f79fe0ffa
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-02 09:22:12 +0530

    Assignment

--- ASSIGNMENTS/Assignment_4/Q3.py
+++ ASSIGNMENTS/Assignment_4/Q3.py
@@ -1,24 +1,55 @@
 # Create a class with attributes _name, _roll_no, and _marks. Provide getter and setter methods with validation (e.g., marks cannot be negative, roll number has to be between 1 & 100 & name cannot be empty)
+
 class Student:
-   pass
 
-   def __init__(self, name, roll_no, marks):
-       self.__name = name  
-       self.__roll_no = roll_no
-       self.__marks = marks
-
-def get_name(self): #getter function
-        return self.__name
-        
-def set_name(self, name): #setter function
-        if name == "":
-              print(f"invalid") 
-        else:
-              print(name)
+    def __init__(self, name, roll_no, marks):
         self.__name = name
+        self.__roll_no = roll_no
+        self.__marks = marks
 
-        if (1 <= roll_no <= 100):
-              print(roll_no)
+    # Getter methods
+    def get_name(self):
+        return self.__name
 
-            else:
-              print("invalid")
+    def get_roll_no(self):
+        return self.__roll_no
+
+    def get_marks(self):
+        return self.__marks
+
+    # Setter methods
+    def set_name(self, name):
+        if name != "":
+            self.__name = name
+        else:
+            print("Invalid name")
+
+    def set_roll_no(self, roll_no):
+        if 1 <= roll_no <= 100:
+            self.__roll_no = roll_no
+        else:
+            print("Invalid roll number")
+
+    def set_marks(self, marks):
+        if marks >= 0:
+            self.__marks = marks
+        else:
+            print("Invalid marks")
+
+
+s1 = Student("Rahul", 10, 85)
+
+# shows the orirginal value
+print(s1.get_name())
+print(s1.get_roll_no())
+print(s1.get_marks())
+
+# update the values
+s1.set_name("Aman")
+s1.set_roll_no(50)
+s1.set_marks(90)
+
+# shows the value after update
+print(s1.get_name())
+print(s1.get_roll_no())
+print(s1.get_marks())
