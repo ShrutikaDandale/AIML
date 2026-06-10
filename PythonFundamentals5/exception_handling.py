d0ab613156dd86d1711a86d8fd205834faa3357d
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-10 15:22:11 +0530

    PythonFundamentals

--- PythonFundamentals5/exception_handling.py
+++ PythonFundamentals5/exception_handling.py
@@ -1,4 +1,5 @@
 # try, except, else, finally
+# suppose program me error aaya hai to program stop hota hai use handle karane ke liye hum exception handling use karake hai taki program ka flow bana rahe
 
 try:
     x = int(input("enter x: "))
@@ -6,6 +7,9 @@ try:
 
 except ZeroDivisionError: 
     print("division buy 0 is now allowed")
+
+except ValueError:
+    print("invalid input")
 
 else:
     print(f"ans = {ans}")       
