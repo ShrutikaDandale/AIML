7ba8e3974982e80dce254b45d0abb2cc6599d936
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-13 15:21:51 +0530

    PythonFundamentals

--- PythonFundamentals5/delete_file.py
+++ PythonFundamentals5/delete_file.py
@@ -1,5 +1,9 @@
-# delete file- this helps to delete the files
-import os 
+# # # delete file- this helps to delete the files
+# import os 
+
+# os.remove("PythonFundamentals5/sample4.txt")
+# python PythonFundamentals5/Pract_prob.py
+
+import os
 
 os.remove("PythonFundamentals5/sample4.txt")
- python PythonFundamentals5/Pract_prob.py  
