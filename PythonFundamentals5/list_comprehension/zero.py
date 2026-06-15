a78d6fa6667ac896cc84cb5c4fa587d397905a3b
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-15 23:02:20 +0530

    PythonFundamentals

--- PythonFundamentals5/list_comprehension/zero.py
+++ PythonFundamentals5/list_comprehension/zero.py
@@ -1,6 +1,8 @@
-# suppose we have a list and we want to convert all the -ve no.s to 0 by using the list comprehension
+# suppose we have a list and we want to convert all the -ve no.s to 0 
+# by using the list comprehension we can do it easily
 
 nums = [-2, 3, -3, 4, -1, 7]
 
 nums = [0 if val<0 else val for val in nums]
 print(nums)
+ 
