7207be7186a490f0969c8c0e1dd630516609588a
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-06-29 16:39:40 +0530

    Practice

---
+++ ASSIGNMENTS/practice/Q2.py
@@ -0,0 +1,7 @@
+a = float(input("enter no. 1 :"))
+b = float(input("enter no. 2 :"))
+
+print("sum = ", a + b)
+print("difference = ", a - b)
+print("product = ", a * b)
+print("quotient = ", a % b)
