638735845a63422400846a57729eb29170d532ea
ShrutikaDandale <shrutikadandale93@gmail.com>
2026-08-05 10:41:11 +0530

    SQL

---
+++ SQL1/ORDERBY_clause.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS instagram;
+USE instagram;
+
+CREATE TABLE users (
+   id INT,
+   age INT,
+   name VARCHAR(23) NOT NULL,
+   email VARCHAR(50) UNIQUE,
+   followers INT DEFAULT 0,
+   following INT, 
+   CONSTRAINT CHECK (age >= 13),
+   PRIMARY KEY (id)
+);
+
+INSERT INTO users
+(id, age, name, email, followers, following)
+VALUES
+(1, 14, "Shanku", "shanku@gmail.com", 123, 145),
+(2, 15, "Kiki", "kiki@gmail.com", 533, 345),
+(3, 16, "Pittu", "pittu@gmail.com", 453, 545),
+(4, 17, "Kookie", "kookie@gmail.com", 343, 745);
+
+
+-- ORDER BY clause do sorting in asc or desc order
+SELECT name, age, followers
+FROM users
+ORDER BY followers ASC;
